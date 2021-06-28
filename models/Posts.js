dfc5a2a2dd44cc6a0951f3d31c85b51197c14b2b
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-06-28 18:44:13 -0400

    Post models updated

---
+++ models/Posts.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Posts extends Model {
+
+}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+
+    User_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'User',
+          key: 'id',
+        },
+      },
+  
+  
+  
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
