c94b7b6f8962661d134076c410caf9ab93d33d6b
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-07 17:24:29 -0400

    routes for login added

--- Controllers/api/login-routes.js
+++ Controllers/api/login-routes.js
@@ -17,5 +17,39 @@ try {
 });
 
 
+// Login
+router.post('/login', async (req, res) => {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  
+});
+
+router.post('/logout', (req, res) => {
+  // When the user logs out, destroy the session
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+
+
 module.exports = router;
 
