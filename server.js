b7644a424f17eea448bb7952b618f89c49791d56
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-07-11 03:41:00 -0400

    some styling

--- server.js
+++ server.js
@@ -7,11 +7,22 @@ const path = require('path');
 const routes = require('./Controllers');
 const sequelize = require('./Config/connection');
 const session = require('express-session');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
+  // cookie: {
+
+  //   maxAge: 86400,
+  // },
+
   resave: false,
   saveUninitialized: true,
+
+  // store: new SequelizeStore({
+  //   db: sequelize,
+  // }),
+
 };
 
 
